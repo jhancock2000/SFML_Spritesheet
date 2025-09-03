3bacd1abd24e35e6c99514aa81435fcad5006bbf
Jonathan Hancock <jhancockdeveloper@gmail.com>
2025-09-02 22:29:05 -0400

    basic drawing code implemented

---
+++ Spritesheet/Texture.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Texture
+{
+public:
+	Texture();
+	~Texture() = default;
+
+
+private:
+};
+
--- Spritesheet/main.cpp
+++ Spritesheet/main.cpp
@@ -1,22 +1,44 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include "Grid.h"
+#include <random>
+#include <cmath>
 using namespace std;
 
 using namespace sf;
 int main()
 {
+    bool click = false;
+    bool click2 = false;
+    srand(time(0));
     sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "Spritesheet Maker");
     window.setFramerateLimit(60);
-     
+    
+    Color currentColour = Color::Black;
+    Color secondColour = Color::White;
     Grid grid;
 
     Grid grid2 = Grid(6, 8, {50, 50});
+
+    //vector<Grid*> grids;
+
+    RectangleShape clear;
+    clear.setPosition({ 0,0 });
+    clear.setSize({ 250, 100 });
+
+
+    for (auto a : grid2.rectangles) {
+        a->setFillColor({ uint8_t(floor(rand() % 255)), uint8_t(floor(rand() % 255)), uint8_t(floor(rand() % 255)) });
+    }
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+
+            click = sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+            click2 = sf::Mouse::isButtonPressed(sf::Mouse::Button::Right);
+            
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
@@ -25,17 +47,43 @@ int main()
         Vector2i mousePos = Mouse::getPosition(window);
         grid.draw(window);
         grid2.draw(window);
+
         for (auto a : grid.rectangles)
         {
             //printf("test");
             if (a->getGlobalBounds().contains({ float(mousePos.x), float(mousePos.y) })) {
                 //grid.setColor({65, 65, 65}, a);
                 RectangleShape r = *a;
-                r.setFillColor({65, 65, 65});
+                r.setFillColor({65, 65, 65, 100});  
 
+
+                if (click) {
+                    a->setFillColor(currentColour);
+                    click = false;
+                }
+                if (click2) {
+                    a->setFillColor(secondColour);
+                    click2 = false;
+                }
                 window.draw(r);
             }
         }
+
+        for (auto a : grid2.rectangles) {
+            if (a->getGlobalBounds().contains({ float(mousePos.x), float(mousePos.y) })) {
+                RectangleShape r = *a;
+                r.setOutlineColor(Color::White);
+                r.setOutlineThickness(2);
+                window.draw(r);
+                if (click) {
+                    currentColour = r.getFillColor();
+                    printf("works!");
+                    click = false;
+                }
+            }
+        }
+
+        window.draw(clear);
 
         window.display();
     }
